a13faa9c76bc24a6860b8a34cc07cb82243e5c8e
Esteban Forero <estebanmff@outlook.com>
2025-01-20 15:06:05 -0500

    Added api.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use std::error::Error;
 
+use repository::Repository;
+use serde::Deserialize;
 mod repository;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    db_url: String,
+    db_token: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv()?;
-    let repository = 
+    let config = envy::from_env::<Config>()?;
+
+    let repository = Repository::new(config.db_url, config.db_token);
 
     Ok(())
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,14 +1,16 @@
 use libsql::{Builder, Database};
 
-struct Repository {
+pub struct Repository {
     db: Database,
 }
 
 impl Repository {
-    async fn new(url: String, token: String) -> Result<(), String> {
+    pub async fn new(url: String, token: String) -> Result<(), String> {
         let db = Builder::new_remote(url, token)
             .build()
             .await
             .map_err(|err| format!("Error creating new remote daabase for libsql: {err}"))?;
+
+        Ok(())
     }
 }
