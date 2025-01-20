ef0448ec2e2e220f6ea50019476c8c128ea68544
Esteban Forero <estebanmff@outlook.com>
2025-01-20 15:01:38 -0500

    repository file added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+mod repository;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenvy::dotenv()?;
+    let repository = 
+
+    Ok(())
 }
